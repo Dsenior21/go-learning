601e72476b32d27067a9e1908278bfc6339e4cb9
Leland Ursu <leland.ursu@gmail.com>
2022-09-08 20:49:23 -0400

    Update main.go

--- lessons/coche/main.go
+++ lessons/coche/main.go
@@ -12,13 +12,20 @@ func suma(a int, b int) {
 
 }
 
+func new() *coche {
+	numPuertas = 4
+}
+
 func (c *coche) puertas() int {
-	i := 4
-	c.numPuertas = i + 1
+	c.numPuertas += 1
 	return c.numPuertas
 }
 
+// make function like puertas above to print the doors
+
 func main() {
+	coche := new() // creates a new car with 4 doors
+	coche.puertas() // adds one door
 	suma(10, 20)
 	fmt.Println()
 
